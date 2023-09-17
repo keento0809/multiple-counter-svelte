bb9ee05e870ec483e00f8804309d4edb1346aa84
KEENTO <handball0321@gmail.com>
2023-09-18 08:52:32 +0900

    Introduced prettier for fixing indent

--- src/lib/components/Counter.svelte
+++ src/lib/components/Counter.svelte
@@ -1,102 +1,109 @@
 <script lang="ts">
-    import type { CounterInfo } from "../../routes/+page.svelte";
+  import type { CounterInfo } from "../../routes/+page.svelte";
 
-    export let counter:CounterInfo;
-    export let handleDeleteCounter:(selectedId:string) => void;
+  export let counter: CounterInfo;
+  export let handleDeleteCounter: (selectedId: string) => void;
 
-    type Action = "INCREMENT" | "DECREMENT" | "RESET"
+  type Action = "INCREMENT" | "DECREMENT" | "RESET";
 
-    const handleChangeCounter:(act:Action) => void = (act) => {
-        switch(act) {
-            case "INCREMENT": {
-                counter.currentCount += 1
-                break
-            }
-            case "DECREMENT": {
-                counter.currentCount -= 1
-                break
-            }
-            case "RESET": {
-                counter.currentCount = 0
-                break
-            }
-            default: {
-                return
-            }
-        }
+  const handleChangeCounter: (act: Action) => void = (act) => {
+    switch (act) {
+      case "INCREMENT": {
+        counter.currentCount += 1;
+        break;
+      }
+      case "DECREMENT": {
+        counter.currentCount -= 1;
+        break;
+      }
+      case "RESET": {
+        counter.currentCount = 0;
+        break;
+      }
+      default: {
+        return;
+      }
     }
+  };
 </script>
 
 <li class="counter">
-    <div>
-        <input type="text" placeholder="new" bind:value={counter.title}>
-    </div>
-    <div class="counter-value">
-        <span>{counter.currentCount}</span>
-    </div>
-    <div>
-        <button class="plus" on:click={() => handleChangeCounter('INCREMENT')}>&plus;</button>
-        <button class="minus" on:click={() => handleChangeCounter('DECREMENT')}>&minus;</button>
-        <button class="reset" on:click={() => handleChangeCounter('RESET')}>0</button>
-    </div>
-    <div>
-        <button class="delete" on:click={() => handleDeleteCounter(counter.id)}>&times;</button>
-    </div>
+  <div>
+    <input type="text" placeholder="new" bind:value={counter.title} />
+  </div>
+  <div class="counter-value">
+    <span>{counter.currentCount}</span>
+  </div>
+  <div>
+    <button class="plus" on:click={() => handleChangeCounter("INCREMENT")}
+      >&plus;</button
+    >
+    <button class="minus" on:click={() => handleChangeCounter("DECREMENT")}
+      >&minus;</button
+    >
+    <button class="reset" on:click={() => handleChangeCounter("RESET")}
+      >0</button
+    >
+  </div>
+  <div>
+    <button class="delete" on:click={() => handleDeleteCounter(counter.id)}
+      >&times;</button
+    >
+  </div>
 </li>
 
 <style>
-    .counter {
-        display: flex;
-        flex-direction: row;
-        justify-content: center;
-        align-items: center;
-        gap: 2rem;
-        padding: 0.5rem 1rem;
-        margin-bottom: 1rem;
-        border-radius: 8px;
-        background-color: rgb(197, 255, 236);
-    }
-    .counter .counter-value {
-        display: block;
-        justify-content: center;
-        align-items: center;
-    }
-    .counter input {
-        padding:0.5rem 0rem 0.5rem 1rem;
-        outline: none;
-        border: 1px solid rgb(168, 163, 163);
-        border-radius: 8px;
-        min-width: 15rem;
-    }
-    .counter span {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-width: 2rem;
-        padding: 0 0.5rem;
-    }
-    .counter button {
-        outline: none;
-        padding: 0.4rem 1.5rem;
-        border-radius: 8px;
-        border: none;
-        cursor: pointer;
-        transition: all 0.3s;
-    }
-    .counter button:hover {
-        transform: scale(1.1);
-    }
-    .counter button.plus {
-        background-color: rgb(242, 36, 36);
-    }
-    .counter button.minus {
-        background-color: rgb(90, 90, 203);
-    }
-    .counter button.reset {
-        background-color: rgb(249, 249, 31);
-    }
-    .counter button.delete {
-        background-color: rgb(31, 200, 251);
-    }
-
+  .counter {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    gap: 2rem;
+    padding: 0.5rem 1rem;
+    margin-bottom: 1rem;
+    border-radius: 8px;
+    background-color: rgb(197, 255, 236);
+  }
+  .counter .counter-value {
+    display: block;
+    justify-content: center;
+    align-items: center;
+  }
+  .counter input {
+    padding: 0.5rem 0rem 0.5rem 1rem;
+    outline: none;
+    border: 1px solid rgb(168, 163, 163);
+    border-radius: 8px;
+    min-width: 15rem;
+  }
+  .counter span {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    min-width: 2rem;
+    padding: 0 0.5rem;
+  }
+  .counter button {
+    outline: none;
+    padding: 0.4rem 1.5rem;
+    border-radius: 8px;
+    border: none;
+    cursor: pointer;
+    transition: all 0.3s;
+  }
+  .counter button:hover {
+    transform: scale(1.1);
+  }
+  .counter button.plus {
+    background-color: rgb(242, 36, 36);
+  }
+  .counter button.minus {
+    background-color: rgb(90, 90, 203);
+  }
+  .counter button.reset {
+    background-color: rgb(249, 249, 31);
+  }
+  .counter button.delete {
+    background-color: rgb(31, 200, 251);
+  }
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,46 +1,51 @@
 <script context="module" lang="ts">
-    export type CounterInfo = {
-        id:string;
-        title: string;
-        currentCount: number;
-    };
+  export type CounterInfo = {
+    id: string;
+    title: string;
+    currentCount: number;
+  };
 </script>
 
 <script lang="ts">
-    import { CounterList,Dashboard } from '$lib';
-    import { v4 as uuid } from 'uuid';
+  import { CounterList, Dashboard } from "$lib";
+  import { v4 as uuid } from "uuid";
 
-    const commonInitialCounterValues:Omit<CounterInfo,'id'> = {
-        title:'new',
-        currentCount:0
-    }
-    let counters:Array<CounterInfo> = [{
-        id:uuid(),
-        ...commonInitialCounterValues
-    }]
-    const handleAddNewCounter:() => void = () => {
-        counters = [...counters,{
-            id:uuid(),
-            ...commonInitialCounterValues
-        }]
-    }
-    const handleDeleteCounter:(selectedId:string) => void = (selectedId) => {
-        counters = counters.filter((c) => c.id !== selectedId)
-    }
+  const commonInitialCounterValues: Omit<CounterInfo, "id"> = {
+    title: "new",
+    currentCount: 0,
+  };
+  let counters: Array<CounterInfo> = [
+    {
+      id: uuid(),
+      ...commonInitialCounterValues,
+    },
+  ];
+  const handleAddNewCounter: () => void = () => {
+    counters = [
+      ...counters,
+      {
+        id: uuid(),
+        ...commonInitialCounterValues,
+      },
+    ];
+  };
+  const handleDeleteCounter: (selectedId: string) => void = (selectedId) => {
+    counters = counters.filter((c) => c.id !== selectedId);
+  };
 </script>
 
 <section class="root">
-    <h1>Multiple Counter</h1>
-    <CounterList bind:counters={counters} handleDeleteCounter={handleDeleteCounter} />
-    <Dashboard bind:counters={counters} handleAddNewCounter={handleAddNewCounter} />
+  <h1>Multiple Counter</h1>
+  <CounterList bind:counters {handleDeleteCounter} />
+  <Dashboard bind:counters {handleAddNewCounter} />
 </section>
 
 <style>
-    .root {
-        min-height: 100vh;
-        display: flex;
-        flex-direction: column;
-        justify-content: flex-start;
-        align-items: center;
-    }
+  .root {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+  }
 </style>
