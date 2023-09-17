92a2cacb07068ccca27ce9c5422dba14fd68a629
KEENTO <handball0321@gmail.com>
2023-09-18 07:47:31 +0900

    Optimized code structures

--- src/lib/components/Counter.svelte
+++ src/lib/components/Counter.svelte
@@ -1,8 +1,11 @@
 <script lang="ts">
     import type { CounterInfo } from "../../routes/+page.svelte";
+
     export let counter:CounterInfo;
     export let handleDeleteCounter:(selectedId:string) => void;
+
     type Action = "INCREMENT" | "DECREMENT" | "RESET"
+
     const handleChangeCounter:(act:Action) => void = (act) => {
         switch(act) {
             case "INCREMENT": {
@@ -24,11 +27,11 @@
     }
 </script>
 
-<li class="counter-container">
+<li class="counter">
     <div>
         <input type="text" placeholder="new" bind:value={counter.title}>
     </div>
-    <div class="counter">
+    <div class="counter-value">
         <span>{counter.currentCount}</span>
     </div>
     <div>
@@ -42,7 +45,7 @@
 </li>
 
 <style>
-    .counter-container {
+    .counter {
         display: flex;
         flex-direction: row;
         justify-content: center;
@@ -53,26 +56,26 @@
         border-radius: 8px;
         background-color: rgb(197, 255, 236);
     }
-    .counter-container .counter {
+    .counter .counter-value {
         display: block;
         justify-content: center;
         align-items: center;
     }
-    .counter-container input {
+    .counter input {
         padding:0.5rem 0rem 0.5rem 1rem;
         outline: none;
         border: 1px solid rgb(168, 163, 163);
         border-radius: 8px;
         min-width: 15rem;
     }
-    .counter-container span {
+    .counter span {
         display: flex;
         justify-content: center;
         align-items: center;
         min-width: 2rem;
         padding: 0 0.5rem;
     }
-    .counter-container button {
+    .counter button {
         outline: none;
         padding: 0.4rem 1.5rem;
         border-radius: 8px;
@@ -80,19 +83,19 @@
         cursor: pointer;
         transition: all 0.3s;
     }
-    .counter-container button:hover {
+    .counter button:hover {
         transform: scale(1.1);
     }
-    .counter-container button.plus {
+    .counter button.plus {
         background-color: rgb(242, 36, 36);
     }
-    .counter-container button.minus {
+    .counter button.minus {
         background-color: rgb(90, 90, 203);
     }
-    .counter-container button.reset {
+    .counter button.reset {
         background-color: rgb(249, 249, 31);
     }
-    .counter-container button.delete {
+    .counter button.delete {
         background-color: rgb(31, 200, 251);
     }
 
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,6 @@
 // place files you want to import through the `$lib` alias in this folder.
+import Counter from "./components/Counter.svelte";
+import CounterList from "./components/CounterList.svelte";
+import Dashboard from "./components/Dashboard.svelte";
+
+export { Counter, CounterList, Dashboard };
