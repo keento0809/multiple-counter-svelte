73a56677bc1e40634b21e3de4b358cb47a76e128
KEENTO <handball0321@gmail.com>
2023-09-17 09:06:55 +0900

    Created a function adding new Counter

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,36 @@
 <script>
-    import Counter from '../lib/components/Counter.svelte'
+    import Counter from '$lib/components/Counter.svelte'
     import Dashboard from '$lib/components/Dashboard.svelte';
+</script>
+
+<script context="module" lang="ts">
+    export type CounterInfo = {
+        id:number;
+        title: string;
+        currentCount: number;
+    };
+    type NewCounterInfo = Omit<CounterInfo,'id'>
+    export let counters:Array<CounterInfo> = [{
+        id:1,
+        title:'default',
+        currentCount:0
+    }]
+    let titleList:Array<string> = []
+    let totalCount = 0;
+
+    const handleAddNewCounter:() => void = () => {
+        counters = [...counters,{
+            id: counters.length + 1,
+            title: '',
+            currentCount: 0
+        }]
+    }
 </script>
 
 <section class="root">
     <h1>Multiple Counter</h1>
     <Counter />
-    <Dashboard />
+    <Dashboard totalCount={totalCount} handleAddNewCounter={handleAddNewCounter} titleList={titleList} />
 </section>
 
 <style>
